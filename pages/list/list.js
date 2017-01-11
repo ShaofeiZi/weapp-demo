6291d54bcd09810cd0dd1278497110f0c600bc3d
zishaofei <zishaofei@concordya.com>
2017-01-11 16:44:10 +0800

    黑黑黑

--- pages/list/list.js
+++ pages/list/list.js
@@ -1,4 +1,13 @@
 // 创建一个页面对象
 // 如果不显式调用，系统也会自动调用
 // 也就是说：此文件可以留空
-Page({})
+Page({
+    // 自定义分享
+    onShareAppMessage: function () {
+        return {
+            title: '啦啦德玛西亚',
+            desc: '黑黑黑',
+            path: '/page/user?id=123'
+        }
+    }
+})
