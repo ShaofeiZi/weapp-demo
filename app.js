0700b3d71afc7f38b7c45bb2a9a955cb53db01c9
zishaofei <zishaofei@concordya.com>
2017-01-11 13:47:37 +0800

    生命周期

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 // 创建一个应用程序对象
 // 如果不显式调用，系统也会自动调用
 // 也就是说：此文件可以留空
-App({})
+App({
+	onLaunch: function () {
+		console.log('App 初始化')
+	},
+	onShow: function () {
+		console.log('App 显示')
+	},
+	onHide: function () {
+		console.log('App 隐藏')
+	}
+})
